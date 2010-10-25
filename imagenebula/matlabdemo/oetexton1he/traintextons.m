ac23ce31725451efcd63ce5313f458fb047b11ea
Baochuan Pang <babypbc@gmail.com>
2010-10-25 11:52:07 +0800

    Add the original image of the H&E demo, and complete the OETEXTON1HE demo. It can train textons both in intensity image and density image.

--- imagenebula/matlabdemo/oetexton1he/traintextons.m
+++ imagenebula/matlabdemo/oetexton1he/traintextons.m
@@ -2,14 +2,17 @@
 % The input image is H&E stained histopathological image. The task is to segment
 % cell nuclei in the image.
 %
-% This demo calculate oriented energy filter bank outputs from 3 channels of the
-% RGB image. And the texton is trained by using the outputs of each pixel as a
+% This demo calculate oriented energy filter bank outputs from 2 channels of the
+% the image H & E. And the texton is trained by using the outputs of each pixel as a
 % vector of features. In this demo, the outputs is not reordered, which means
 % that the textons is not rotation free. Also, we calculate the OE filter bank
 % outputs in the orientations in [0, pi]. 
 
 %% Reading the RGB image
-im = imread('part.png');
+im(:, :, 1) = imread('H.png');
+im(:, :, 2) = imread('E.png');
+im = double(im) / 255;
+im = log(1) - log(im); % comment this line to train textons on density image
 
 %% Parameters
 norients = 6;
@@ -17,7 +20,7 @@ startsigma = 0.5;
 nscales = 7;
 scalingstep = 1.2;
 elong = 3;
-ntexton = 32;
+ntexton = 8;
 
 %% Calcualte the filter bank outputs
 [fb, thetas, scales] = filterbankoe(norients, startsigma, nscales, ...
