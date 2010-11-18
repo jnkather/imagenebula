cb697456f17fc67c1886cfb946b9e9a028c4711b
Baochuan Pang <babypbc@gmail.com>
2010-11-18 15:40:16 +0800

    Add function BWCENTROID for calculating centroid coordinates of region or regions specified by a BW mask.

---
+++ imagenebula/matlab/utils/images/bwcentroid.m
@@ -0,0 +1,64 @@
+function [cr, cc] = bwcentroid(bw)
+%BWCENTROID returns the centroid coordinate of the region(s) specified by BW.
+%
+% [CR, CC] = BWCENTROID(BW)
+%
+% This function calculate the centroid coordinate of the region(s) specified by 
+% BW. The region is specified by BW>1.
+%
+% INPUTS
+%  BW		- Region mask or masks(cell of images), region is specified by BW>1.
+%
+% OUTPUTS
+%	CR      - Centroid row index
+%	CC		- Centroid column index
+%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING, USING OR
+% MODIFYING.
+%
+% By downloading, copying, installing, using or modifying this
+% software you agree to this license.  If you do not agree to this
+% license, do not download, install, copy, use or modifying this
+% software.
+% 
+% Copyright (C) 2010-2010 Baochuan Pang <babypbc@gmail.com>
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Lesser General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful, but
+% WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+% General Public License for more details.
+%
+% You should have received a copy of the GNU Lesser General Public License
+% along with this program.  If not, see
+% <http://www.gnu.org/licenses/>.
+%
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&&&&&&&
+
+if iscell(bw)
+	% cell of masks
+	cr = zeros(numel(bw), 1);
+	cc = zeros(numel(bw), 1);
+	
+	for i = 1 : numel(bw)
+		[rs, cs] = find(bw{i} > 0);
+		cr(i) = mean(rs);
+		cc(i) = mean(cs);
+	end
+else
+	% single mask
+	[rs, cs] = find(bw > 0);
+	
+	cr = mean(rs);
+	cc = mean(cs);	
+end
+
+
